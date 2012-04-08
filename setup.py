41e9b1e185943b4a09d9418170c244aec797fcba
Alex Clark <aclark@aclark.net>
2012-04-07 21:00:12 -0400

    Alphasort

--- setup.py
+++ setup.py
@@ -1,34 +1,39 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages
+from setuptools import setup
 import os
 
-version = '0.3'
+VERSION = '0.3'
 
-def read(input):
-    return open(input, 'rw').read()
 
-setup(name='collective.package',
-      version=version,
-      description="Sample package to demonstrate development of add-ons for the Plone CMS",
-      long_description=read('README.txt') + read('docs/HISTORY.txt'),
-      classifiers=[
-        "Framework :: Plone",
-        "Programming Language :: Python",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-        ],
-      keywords='',
-      author='Alex Clark',
-      author_email='aclark@aclark.net',
-      url='http://svn.plone.org/svn/collective/collective.package/',
-      license='GPL',
-      packages=find_packages(),
-      namespace_packages=['collective'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'setuptools',
-      ],
-      entry_points="""
-      [z3c.autoinclude.plugin]
-      target = plone
-      """,
-      )
+setup(
+    author='Alex Clark',
+    author_email='aclark@aclark.net',
+    classifiers=[
+      "Framework :: Plone",
+      "Programming Language :: Python",
+      "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    description="Sample package to demonstrate development of \
+        add-ons for the Plone CMS",
+    entry_points={
+        'z3c.autoinclude.plugin': 'target = plone',
+    },
+    include_package_data=True,
+    install_requires=[
+        'setuptools',
+    ],
+    long_description=(
+        open('README.rst').read() +
+        open(os.path.join('docs', 'HISTORY.txt'))
+    ),
+    keywords='plone example package',
+    license='ZPL',
+    name='collective.package',
+    namespace_packages=[
+        'collective'
+    ],
+    packages=find_packages(),
+    url='http://collective.github.com/collective.package/',
+    version=VERSION,
+    zip_safe=False,
+)
